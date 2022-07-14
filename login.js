c62932f006a87fec479eb315dd0393f6f6d8e744
Jorge Fernandez <jorgelfc1989@gmail.com>
2022-07-14 12:18:41 -0400

    made minor changes to login.js file

--- login.js
+++ login.js
@@ -8,27 +8,53 @@ function retrieveData(){
     }
 }
 
+// function login(){
+//     let email = document.getElementById("inputEmail").value;
+//     let password = document.getElementById("inputPassword").value;
+//     let emailSaved = localStorage.getItem("Email");
+//     let pwdSaved = localStorage.getItem("Password");
+
+//     if (email.length === 0 || password.length === 0) {
+//         attempt--;
+//         alert("Please enter user name and password! You have left "+ attempt+ " attempts");
+
+//     if (attempt === 0){
+//         document.getElementById("inputEmail").disabled = true;
+//         document.getElementById("inputPassword").disabled = true;
+//         document.getElementById("submit").disabled = true;
+//     }
+//     } else {
+//         if(email === emailSaved && password === pwdSaved) {
+//             alert("Credentials validated");
+//             location.assign("index.html");
+//         } else {
+//             alert("Sorry! Wrong Credentials, please try again.")
+//         }
+//     }
+// }
+
 function login(){
-    let email = document.getElementById("inputEmail").value;
-    let password = document.getElementById("inputPassword").value;
-    let emailSaved = localStorage.getItem("Email");
-    let pwdSaved = localStorage.getItem("Password");
-
-    if (email.length === 0 || password.length === 0) {
-        attempt--;
-        alert("Please enter user name and password! You have left "+ attempt+ " attempts");
-
-    if (attempt === 0){
+    let attempt = 1;
+    var email = document.getElementById("inputEmail").value;
+    var password = document.getElementById("inputPassword").value;
+    var emailSaved = localStorage.getItem('Email');
+    var pwdSaved = localStorage.getItem('Password');
+    if(email.length===0 || password.length===0){
+       attempt--;// Decrementing by one.
+    alert("Please enter user name and password! You have "+attempt+" attempts left");
+      //Disabling fields after 3 attempts.
+      if( attempt == 0){
         document.getElementById("inputEmail").disabled = true;
         document.getElementById("inputPassword").disabled = true;
         document.getElementById("submit").disabled = true;
+      }
     }
-    } else {
-        if(email === emailSaved && password === pwdSaved) {
-            alert("Credentials validated");
-            location.assign("index.html");
-        } else {
-            alert("Sorry! Wrong Credentials, please try again.")
-        }
+    else{
+       if(email==emailSaved && password==pwdSaved){
+         alert('Credentials validated!');
+         location.assign('Home.html');
+      }else{
+        alert('Oops! Wrong credentials! Please try again');
+      }
     }
-}
+    }
